e2be8ee41bc47dc3e3c4ab4e0b1a96fab9071fe8
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-22 15:42:33 +0200

    Edit Model (Project & Type) -> belongsTo & hasMany

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,6 +14,7 @@ class Project extends Model
     protected $table = 'projects';
 
     protected $fillable = [
+        'type_id',
         'title',
         'description',
         'buyer',
@@ -22,4 +23,8 @@ class Project extends Model
         'programming_languages',
         'link'
     ];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
